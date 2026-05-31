8cf94acdd0941f5250bfc9407ce0e0bcc69e9c50
acbhaskar1 <acbhaskar1@gmail.com>
2026-05-31 12:53:36 +0530

    Just added welcome text

--- src/com/akshaya/App.java
+++ src/com/akshaya/App.java
@@ -1,24 +1,8 @@
-//Program to input two numbers and calculate sum
-
 package com.akshaya;
 
-import java.util.Scanner;
-
 public class App {
-
-    public static int inputValues(Scanner sc, String prompt) {
-        System.out.print(prompt);
-        int val = sc.nextInt();
-        return val;
-    }
-    public static int calculateSum(int value1,int value2){
-        var sum = value1 + value2;
-        return sum;
-    }
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        int val1 = inputValues(sc, "Enter first value : ");
-        int val2 = inputValues(sc, "Enter second value : ");
-        System.out.println("The sum of given values is : " + calculateSum(val1, val2));
+        //solution for the exercise 06_02 branch question
+        System.out.println("Hey YOU!, Welcome to jave practice lessons or chapters");
     }
 }
