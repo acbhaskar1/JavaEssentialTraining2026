4103937d5401f00e2f4f040c93862bcbb10567c5
acbhaskar1 <acbhaskar1@gmail.com>
2026-06-01 03:20:44 +0530

    The new practice lesson

--- src/com/akshaya/App.java
+++ src/com/akshaya/App.java
@@ -1,8 +1,6 @@
-//Program to input two numbers and calculate sum
+// Debugging and Exceptions
 
 package com.akshaya;
-
-import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
