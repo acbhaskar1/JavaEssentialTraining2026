be2556c99f42c028b738a5c203846d8d2a5da4a8
acbhaskar1 <acbhaskar1@gmail.com>
2026-06-01 04:28:13 +0530

    Reading the data or text from a file

--- src/com/akshaya/App.java
+++ src/com/akshaya/App.java
@@ -2,8 +2,27 @@
 
 package com.akshaya;
 
+import java.io.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-       //write your code here
+       try {
+           File file = new File("hello.txt");
+           System.out.println("The hello.txt file exist: " + file.exists());
+
+           try {
+               FileReader reader = new FileReader(file);
+               BufferedReader buffReader= new BufferedReader(reader);
+               String text = buffReader.readLine();
+               System.out.println(text);
+           } catch (IOException e) {
+               e.printStackTrace();
+           }
+
+       } catch (Exception e) {
+           e.printStackTrace();
+       }
+        System.out.println("I am not dead YET!");
     }
+
 }
