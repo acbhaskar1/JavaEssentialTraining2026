6ca005361ec35d0df6667e9764c9af4a0383a65d
acbhaskar1 <acbhaskar1@gmail.com>
2026-05-31 11:40:23 +0530

    Modular way to calculate sum

--- src/com/akshaya/App.java
+++ src/com/akshaya/App.java
@@ -1,8 +1,24 @@
+//Program to input two numbers and calculate sum
+
 package com.akshaya;
 
+import java.util.Scanner;
+
 public class App {
+
+    public static int inputValues(Scanner sc, String prompt) {
+        System.out.print(prompt);
+        int val = sc.nextInt();
+        return val;
+    }
+    public static int calculateSum(int value1,int value2){
+        var sum = value1 + value2;
+        return sum;
+    }
     public static void main(String[] args) throws Exception {
-        // write your code here try removing below line number 6 code and start writing
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        int val1 = inputValues(sc, "Enter first value : ");
+        int val2 = inputValues(sc, "Enter second value : ");
+        System.out.println("The sum of given values is : " + calculateSum(val1, val2));
     }
 }
