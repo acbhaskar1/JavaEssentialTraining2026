388ed55d8e9a777a06610afc8cb18977ec885ec6
acbhaskar1 <acbhaskar1@gmail.com>
2026-05-31 11:23:43 +0530

    Practice lesseon 06_01

---
+++ src/com/akshaya/App.java
@@ -0,0 +1,8 @@
+package com.akshaya;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // write your code here try removing below line number 6 code and start writing
+        System.out.println("Hello, World!");
+    }
+}
