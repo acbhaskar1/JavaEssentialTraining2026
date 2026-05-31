27bb32467f3fb6aaa575ab15d7538befac0c089a
acbhaskar1 <acbhaskar1@gmail.com>
2026-06-01 03:38:04 +0530

    Debugging or exception hadling lesson

--- src/com/akshaya/App.java
+++ src/com/akshaya/App.java
@@ -1,11 +1,17 @@
-//Program to input two numbers and calculate sum
+//Debugging or Exception
 
 package com.akshaya;
 
-import java.util.Scanner;
-
 public class App {
     public static void main(String[] args) throws Exception {
-       //write your code here
+        try {
+             String str = null;
+             var extract = str.substring(1);
+           } catch (Exception e) {
+             e.printStackTrace();
+           }
+        System.out.println("I haven't dead YET! ");
     }
 }
+
+
