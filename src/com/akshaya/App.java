2a5a053dfc3d87d154141451d4ccdbacd28a12e5
acbhaskar1 <acbhaskar1@gmail.com>
2026-05-31 12:47:16 +0530

    exercise file

--- src/com/akshaya/App.java
+++ src/com/akshaya/App.java
@@ -5,20 +5,7 @@ package com.akshaya;
 import java.util.Scanner;
 
 public class App {
-
-    public static int inputValues(Scanner sc, String prompt) {
-        System.out.print(prompt);
-        int val = sc.nextInt();
-        return val;
-    }
-    public static int calculateSum(int value1,int value2){
-        var sum = value1 + value2;
-        return sum;
-    }
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        int val1 = inputValues(sc, "Enter first value : ");
-        int val2 = inputValues(sc, "Enter second value : ");
-        System.out.println("The sum of given values is : " + calculateSum(val1, val2));
+       //write your code here
     }
 }
