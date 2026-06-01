1b8de4392533184d0b4ce46f671b7363acda04bb
acbhaskar1 <acbhaskar1@gmail.com>
2026-06-01 16:29:44 +0530

    usage of model and setter getter methods

--- src/com/akshaya/App.java
+++ src/com/akshaya/App.java
@@ -1,10 +1,21 @@
 // adding models to the programming style
 package com.akshaya;
 
-import java.util.Scanner;
+import com.akshaya.model.ClothingItem;
+
+import java.text.NumberFormat;
 
 public class App {
     public static void main(String[] args) throws Exception {
-       //write your code here
+       var item = new ClothingItem();
+       item.setType("Shirt");
+       item.setSize("M");
+       item.setPrice(99.99);
+       item.setQty(4);
+
+       var totalPrice = item.getPrice() * item.getQty();
+       var formatter = NumberFormat.getCurrencyInstance();
+       var output = String.format("The total selling price of %s with size %s for a %d quantity is %s", item.getType(), item.getSize(), item.getQty(), formatter.format(totalPrice));
+        System.out.println(output);
     }
 }
--- src/com/akshaya/model/ClothingItem.java
+++ src/com/akshaya/model/ClothingItem.java
@@ -1,5 +1,40 @@
 package com.akshaya.model;
 
 public class ClothingItem {
-    // your code starts here
+    private String type;
+    private String size;
+    private double price;
+    private int qty;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
 }
