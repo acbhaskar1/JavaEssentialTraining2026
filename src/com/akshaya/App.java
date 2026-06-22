7c65490038be628163370a25d54c92ddc21c9a08
acbhaskar1 <acbhaskar1@gmail.com>
2026-06-22 17:44:41 +0530

    updated the small changes

--- src/com/akshaya/App.java
+++ src/com/akshaya/App.java
@@ -1,8 +1,5 @@
 // First java program print Hello world! in the console
 package com.akshaya;
-
-import java.sql.SQLOutput;
-
 public class App {
     public static void main(String[] args) {
        //write your code here
