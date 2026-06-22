df78544502872ec1d3298bf32ab269d67700582d
acbhaskar1 <acbhaskar1@gmail.com>
2026-06-22 17:41:55 +0530

    The first program

--- src/com/akshaya/App.java
+++ src/com/akshaya/App.java
@@ -1,10 +1,11 @@
-// adding models to the programming style
+// First java program print Hello world! in the console
 package com.akshaya;
 
-import java.util.Scanner;
+import java.sql.SQLOutput;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
        //write your code here
+        System.out.println("Hello World!");
     }
 }
